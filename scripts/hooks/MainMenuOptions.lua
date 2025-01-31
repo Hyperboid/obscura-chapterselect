2c9aaa5c7ba9e3db75917cfa4c20a2cdb974e88f
hyperboid <23138363+polypoyo@users.noreply.github.com>
2025-01-31 04:31:14 -0500

    Remove example code intended for Eclipse

--- scripts/hooks/MainMenuOptions.lua
+++
@@ -1,9 +0,0 @@
----@class MainMenuOptions:MainMenuOptions
-local MainMenuOptions, super = Class("MainMenuOptions")
-
-function MainMenuOptions:initializeOptions()
-    super.initializeOptions(self)
-    self:registerConfigOption("general", "Esc for Chapters", "escapeChapterSelect")
-end
-
-return MainMenuOptions
